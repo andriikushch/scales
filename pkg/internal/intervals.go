35bf581dcea331f8125686419cfb9dc01944a5de
andriikushch <andrii.kushch@gmail.com>
2025-03-01 14:31:48 +0100

    Add intervals

---
+++ pkg/internal/intervals.go
@@ -0,0 +1,93 @@
+package internal
+
+const (
+	// IUnison Unison (Perfect Unison): 0 half steps.
+	IUnison = 0
+
+	// Im2 Minor Second (m2): 1 half step.
+	Im2 = 1 * HalfStep
+	// IM2 Major Second (M2): 2 half steps.
+	IM2 = 2 * HalfStep
+	// IA2 Augmented Second (A2): 3 half steps.
+	IA2 = 3 * HalfStep
+	// ID2 Diminished Second (D2): 0 half steps.
+	ID2 = 0 * HalfStep
+
+	// Im3 Minor Third (m3): 3 half steps.
+	Im3 = 3 * HalfStep
+	// IM3 Major Third (M3): 4 half steps.
+	IM3 = 4 * HalfStep
+	// IA3 Augmented Third (A3): 5 half steps.
+	IA3 = 5 * HalfStep
+	// ID3 Diminished Third (D3): 2 half steps.
+	ID3 = 2 * HalfStep
+
+	// IP4 Perfect Fourth (P4): 5 half steps.
+	IP4 = 5 * HalfStep
+	// IA4 Augmented Fourth (A4) / Tritone: 6 half steps.
+	IA4 = 6 * HalfStep
+	// ID4 Diminished Fourth (D4): 4 half steps.
+	ID4 = 4 * HalfStep
+
+	// IP5 Perfect Fifth (P5): 7 half steps.
+	IP5 = 7 * HalfStep
+	// IA5 Augmented Fifth (A5): 8 half steps.
+	IA5 = 8 * HalfStep
+	// ID5 Diminished Fifth (D5): 6 half steps.
+	ID5 = 6 * HalfStep
+
+	// Im6 Minor Sixth (m6): 8 half steps.
+	Im6 = 8 * HalfStep
+	// IM6 Major Sixth (M6): 9 half steps.
+	IM6 = 9 * HalfStep
+	// IA6 Augmented Sixth (A6): 10 half steps.
+	IA6 = 10 * HalfStep
+	// ID6 Diminished Sixth (D6): 7 half steps.
+	ID6 = 7 * HalfStep
+
+	// Im7 Minor Seventh (m7): 10 half steps.
+	Im7 = 10 * HalfStep
+	// IM7 Major Seventh (M7): 11 half steps.
+	IM7 = 11 * HalfStep
+	// IA7 Augmented Seventh (A7): 12 half steps.
+	IA7 = 12 * HalfStep
+	// ID7 Diminished Seventh (D7): 9 half steps.
+	ID7 = 9 * HalfStep
+
+	// IOctave Octave (Perfect Octave): 12 half steps.
+	IOctave = 12 * HalfStep
+
+	// Im9 Minor Ninth (m9): 13 half steps.
+	Im9 = 13 * HalfStep
+	// IM9 Major Ninth (M9): 14 half steps.
+	IM9 = 14 * HalfStep
+	// IA9 Augmented Ninth (A9): 15 half steps.
+	IA9 = 15 * HalfStep
+	// ID9 Diminished Ninth (D9): 12 half steps.
+	ID9 = 12 * HalfStep
+
+	// Im10 Minor Tenth (m10): 15 half steps.
+	Im10 = 15 * HalfStep
+	// IM10 Major Tenth (M10): 16 half steps.
+	IM10 = 16 * HalfStep
+	// IA10 Augmented Tenth (A10): 17 half steps.
+	IA10 = 17 * HalfStep
+	// ID10 Diminished Tenth (D10): 14 half steps.
+	ID10 = 14 * HalfStep
+
+	// IP11 Perfect Eleventh (P11): 17 half steps.
+	IP11 = 17 * HalfStep
+	// IA11 Augmented Eleventh (A11): 18 half steps.
+	IA11 = 18 * HalfStep
+	// ID11 Diminished Eleventh (D11): 16 half steps.
+	ID11 = 16 * HalfStep
+
+	// Im13 Minor Thirteenth (m13): 20 half steps.
+	Im13 = 20 * HalfStep
+	// IM13 Major Thirteenth (M13): 21 half steps.
+	IM13 = 21 * HalfStep
+	// IA13 Augmented Thirteenth (A13): 22 half steps.
+	IA13 = 22 * HalfStep
+	// ID13 Diminished Thirteenth (D13): 19 half steps.
+	ID13 = 19 * HalfStep
+)
